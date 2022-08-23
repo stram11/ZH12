33c86637a157ed1eed6d2f294d40d97d1d307da4
stram11 <108708139+stram11@users.noreply.github.com>
2022-08-23 11:39:08 +0700

    Update Player.cs

--- zh-main/Assets/Scripts/Player.cs
+++ zh-main/Assets/Scripts/Player.cs
@@ -11,12 +11,12 @@ public class Player : MonoBehaviour
     int hp = 100;
     Image healthbar_border;
 
-    
+
     // Start is called before the first frame update
     void Start()
     {
         asource = GetComponent<AudioSource>();
-        healthbar_border =   gameObject.Find("healthbar_bg").GetComponent<Image>();
+        healthbar_border = gameObject.Find("healthbar_bg").GetComponent<Image>();
     }
 
     // Update is called once per frame
@@ -27,14 +27,22 @@ public class Player : MonoBehaviour
             asource.PlayOneShot(firearm);
         }
 
-        healthbar_border.fillAmount = hp;
+        healthbar_border.fillAmount = hp / 100;
     }
 
     void OntriggerEnter(Collider other)
     {
-        if(other.gameObject.CompareTag("Zombie"))
+        if (other.gameObject.CompareTag("Zombie"))
         {
             hp -= 20;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Zombie"))
+        {
+            hp -= 0.01f;
         }
     }
 }
